009ce028802c333f5624587b5262d701e3a0b0dc
mkolh <mkolhede@gmail.com>
2015-08-16 15:30:51 -0700

    Now works! Add the channel of your choosing to the server.js file, then load the bot, type 'learn' and a middle eastern country name, than type the name of the thing you want to learn about

--- server.js
+++ server.js
@@ -21,93 +21,97 @@ var bot = new irc.Client(config.server, config.botName, {
 // });s
 
 var tick = 0;
+var keys = [];		
+var country = {};
 
 bot.addListener("message", function(from, to, text, message) {
 
-	var messages = message.args[1].toLowerCase();
+	var messages = message.args[1].toLowerCase().split(" ");
 
-	if(messages === "learn"){
+	if(messages[0] === "learn"){
 		
-		var iraq = {};
+		//rename to the proper convention
+		var countryName = messages[1].substring(0, 2) + "-" + messages[1];
 
 		var fs_readFile = q.denodeify(fs.readFile)
-		var promise = fs_readFile('middle-east/iraq.json', 'utf8')
+		var promise = fs_readFile('middle-east/' + countryName + ".json", 'utf8')
 
 		.then(function(data){
 			
 			//this object will contain the full JSON file and the initial list
 
 			//turns data (already dealt with buffer) to JSON
-			iraq = JSON.parse(data);
+			country = JSON.parse(data);
 
-			search(iraq, messages);
+			var tableOfContents = [];
+
+			for(p in country){
+				tableOfContents.push(p);
+			};
+
+			tableOfContents = tableOfContents.join("\n");
+			bot.say(config.channels[0], tableOfContents);
 
 			console.log('donzo');
 			})
 		.done();
 
-	};
+	}else{
+		//search with mesages
+		//if country is empty message return "Say Learn to Learn"
+	console.log("Searching..." + country);
+	search(country, messages[0]);
+	}
 
 
 });
 
-/*
-*@obj = current json object, iraq.comm etc (promise object)
-*@list = array of strings for the current properties of the json object
-*@message = the typed response to the previous function call
-*/
-function search(obj, messages){
+//function will recursively search
+function search(obj, name) {
 
-	console.log("Another go around");
-	console.log(messages);
+    for (var key in obj) {
 
+        if (obj.hasOwnProperty(key)) {
+            if (key === name) {
+            	
+            	//found the requested key! Let's see if it's an object,
+            	//if so, provide the user the availible sub-categories
+            	//if it has a text property, return the text!
 
-	var keys = [];
+                if ("object" == typeof (obj[key])) {
+                    var keys = [];
 
-	for(prop in obj){
-		keys.push(prop);
-	};	
-	
-
-	console.log(keys);
-
-	//this is crashing it
-	if('string' === typeof obj.messages){
-
-		console.log('found the text');
-
-		bot.say(config.channels[0], obj.text);
-
-	}else{
-
-		//display new choices
-		
-		
-		keys = keys.join("\n");
-
-		bot.say(config.channels[0], keys);
-
-		//set listener for next message
-		bot.addListener("message", function(from, to, text, message) {
-
-			var newMessage = message.args[1].toLowerCase();
-		
-			if(tick < 1){
-				tick++;
-				search(obj[newMessage], newMessage);
-			}else{
-				
-				console.log(obj[newMessage].text);
-
-				search(obj, newMessage);
-			};
-
-		})
+                    for (var prop in obj[key]) {
+                        keys.push(prop);
+                    }
 
 
-	}
 
+                    if(obj[key].text){
+                    	//found text property! Return the text!
+                    	console.log("FOUND TEXT!!!");
+                    	bot.say(config.channels[0], obj[key].text);
+
+                    }else{
+                    	//provide user list of availible sub-categories
+                    	keys = keys.join("\n");
+						bot.say(config.channels[0], keys);
+                    };
+                }else{
+                	//no sub-categories
+                	console.log(obj[name]);
+                	bot.say(config.channels[0], obj[name]);
+                }
+            } else {
+                if ("object" == typeof (obj[key])) {
+                	//recursive search
+                    search(obj[key], name);
+                }
+            }
+        }
+    }
 }
+
 
 bot.addListener('error', function(message) {
     console.log('error: ', message);
